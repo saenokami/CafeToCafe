80e70ced589611e84d61e6aaac1e8bebc9dd7f14
saenokami <165577235+saenokami@users.noreply.github.com>
2024-06-27 13:27:35 +0000

    Initial Commit

---
+++ app/controllers/public/shops_controller.rb
@@ -0,0 +1,22 @@
+class Public::ShopsController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :admins
+  
+  devise_for :users, controllers: {
+    registrations: 'public/registrations'
+  }
+
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+    resources :shops, only: [:new, :index, :show, :create, :edit, :update]
+    resources :posts, only: [:new, :index, :show, :create, :edit, :update, :destroy]
+    resources :users, only: [:mypage, :edit, :show, :update, :destroy]
+  end
+
+  root to: "public/homes#top"
+end
