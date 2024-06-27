9bdbf179aff1ac7d894a29f6e17d4cd17f2db9c2
saenokami <165577235+saenokami@users.noreply.github.com>
2024-06-27 15:17:27 +0000

    [Add]ヘッダーの表示切り替え

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -8,6 +8,6 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :user_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :first_name, :last_name, :first_name_kana, :last_name_kana, :user_name])
   end
 end
